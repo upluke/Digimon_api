cb20f32818f501e96c8d0dacf3de64a43143f854
upluke <lukeforadream@gmail.com>
2022-06-16 17:20:51 -0700

    fetch digimon with lmt 10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import { Component } from "react";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state={digimon:[]}
+  }
+
+  componentDidMount(){
+    fetch('https://digimon-api.vercel.app/api/digimon/?_limit=10')
+    .then((response)=>response.json())
+    .then((data)=>{
+      data=data.slice(0, 10)
+      this.setState({digimon:data}) 
+    })
+    // .then((data)=> this.setState({digimon:data}))
+  }
+
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-            test
-        </header>
+        <ul>
+          {this.state.digimon.map((monster)=>{
+            return (
+              <div key={monster.name}>
+                  <h1 >{monster.name}</h1>
+                  <img src={monster.img} />
+              </div>
+            ) 
+        })}
+        </ul>
       </div>
     );
     }
