19e1eb120b1759568d4280d9e1f05e067ad77a1d
upluke <lukeforadream@gmail.com>
2022-06-17 21:55:45 -0700

    delete input bug fixed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import { Component } from "react";
 
 class App extends Component {
   constructor(){
+    console.log("1")
     super()
-    this.state={digimon:[]}
+    this.state={
+      digimon:[],
+      searchItem:""
+    }
   }
 
   componentDidMount(){
+    console.log("2")
     fetch('https://digimon-api.vercel.app/api/digimon/?_limit=10')
     .then((response)=>response.json())
     .then((data)=>{
@@ -15,23 +20,22 @@ class App extends Component {
     })
     // .then((data)=> this.setState({digimon:data}))
   }
-
+ 
   render(){
+    console.log('3')
+    const filteredDigimon=this.state.digimon.filter((dm)=>{
+      return dm.name.toLocaleLowerCase().includes(this.state.searchItem)
+    })
 
     return (
       <div className="App">
         <input 
           onChange={(e)=>{
-            const searchItem=e.target.value.toLocaleLowerCase()
-            let filteredDigimon=this.state.digimon.filter((dm)=>{
-                  return dm.name.toLocaleLowerCase().includes(searchItem)
-            })
-            this.setState({digimon:filteredDigimon})
-            console.log(filteredDigimon, "%%%%%")
+            this.setState({searchItem: e.target.value.toLocaleLowerCase()})
           }} 
           placeholder="Search your digitmon..." />
         <ul>
-          {this.state.digimon.map((monster)=>{
+          {filteredDigimon.map((monster)=>{
             return (
               <div key={monster.name}>
                   <h1 >{monster.name}</h1>
