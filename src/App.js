a97986c640abe224d683021f1981482fa365174f
upluke <lukeforadream@gmail.com>
2022-06-21 12:17:28 -0700

    css can be accessed anywhere

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import './App.css'
 import SearchBox from "./components/search-box/search-box.component";
 import CardList from "./components/card-list/card-list.component";
 
